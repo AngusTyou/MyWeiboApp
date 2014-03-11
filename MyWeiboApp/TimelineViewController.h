d6d93e3df5d3805401d8b7f41223317c3d0d3e98
AngusTyou <z.angus0121@gmail.com>
2014-03-11 23:21:45 +0900

    weibo app

---
+++ MyWeiboApp/TimelineViewController.h
@@ -0,0 +1,14 @@
+//
+//  TimelineViewController.h
+//  MyWeiboApp
+//
+//  Created by 張 彪 on 12-12-15.
+//  Copyright (c) 2012年 張 彪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineViewController : UITableViewController
+- (IBAction)pressComposeButton:(id)sender;
+
+@end
